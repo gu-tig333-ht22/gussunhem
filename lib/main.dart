e37e0c5c5f0b0a1dec8e370aa31c9e2d49ad6ddb
H-Sundberg <77834886+H-Sundberg@users.noreply.github.com>
2022-09-09 14:22:34 +0200

    Last commit for week1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-// //to-do-list
-// statefulwidget for unchecking boxes
-
-//import 'dart:html';
-
+import 'dart:html';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,131 +8,130 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'TODO',
-        theme: ThemeData(primarySwatch: Colors.deepPurple), //rgb
-        home: MyHomePage());
+    return MaterialApp(title: 'TODO app', home: MyHomePage());
   }
 }
-
-// HomePage
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("TODO list!"),
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 71, 30, 140)),
-      body: /*Container(
-          color: Colors.blue,
-          padding: EdgeInsets.all(40)),*/
-
-          Center(
-        child: Column(
-          // MainAxisAlignment.something
-          children: [
-            // multiple values
-            Progress(),
-            TaskList(),
-            ElevatedButton(
-              child: Text('Add something to the TODO list'),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.deepPurple[300],
-                onPrimary: Colors.white,
-                padding: EdgeInsets.all(30),
-              ),
-              onPressed: () {},
+        title: Text("TODO list!"),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 42, 53, 158),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.white,
             ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TodoList(),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {}, //add TODO-item to list
+        backgroundColor: Color.fromARGB(255, 42, 53, 158),
         tooltip: 'Add a TODO',
         child: const Icon(Icons.add),
+        onPressed: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SecondPage())),
       ),
     );
   }
 }
 
-// classes
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("TODO list!"),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 42, 53, 158),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_rounded, color: Colors.white),
+            onPressed: () => Navigator.of(context).pop(),
+          )),
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              decoration:
+                  InputDecoration(hintText: 'What would you like to add?'),
+            ),
+            Icon(
+              Icons.add,
+              color: Colors.deepPurple,
+              size: 24,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-class Progress extends StatelessWidget {
-  // change name of class
+class TodoList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text("Here below you can add stuff to your TODO list;", // change color
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 62, 71, 154),
-            )),
-        LinearProgressIndicator(value: 7.0), // fade
+        TodoItem(
+          label: "Create checkboxes",
+        ),
+        TodoItem(
+          label: "Add class navigating to second page",
+        ),
+        TodoItem(
+          label: "Change from default color to RGB",
+        ),
+        TodoItem(
+          label: "Add app bar for second page",
+        ),
+        TodoItem(
+          label: "Add three dots in appbar",
+        ),
+        TodoItem(
+          label: "Make a PR on github",
+        ),
+        TodoItem(
+          label: "Enjoy weekend!",
+        ),
       ],
     );
   }
 }
 
-class TaskList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TaskItem(
-          label: "This is something I want to add",
-        ), // add bold text, wider between tasks
-        TaskItem(
-          label: "TODO 2",
-        ), // add cross in right corner to remove task + remove from list
-        TaskItem(
-          label: "TODO 3",
-        ),
-        TaskItem(
-          label: "TODO 4",
-        ),
-        TaskItem(
-          label: "TODO 5",
-        ),
-      ],
-    );
-  }
-}
-
-class TaskItem extends StatelessWidget {
-  // change to statefull
+class TodoItem extends StatelessWidget {
   final String label;
 
-  const TaskItem({Key? key, required this.label}) : super(key: key);
+  const TodoItem({Key? key, required this.label}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Container(
+        child: Row(
       children: [
-        Checkbox(onChanged: null, value: false), // onChanged: cross/uncrossed
+        Checkbox(
+          onChanged: (val) {},
+          value: true,
+        ), // value: false for unchecked checkboxes
         Text(label,
             style: TextStyle(
-              fontSize: 17.0,
-              fontWeight: FontWeight.w500,
-              color: Colors.deepPurple[600],
-              fontStyle: FontStyle.italic,
-            )), // download a font from google
+              fontSize: 16.0,
+              fontWeight: FontWeight.w400,
+              color: Color.fromARGB(255, 36, 36, 143),
+            )),
+        Icon(Icons.close, color: Colors.deepPurple, size: 18),
       ],
-    );
+    ));
   }
 }
-
-// Add announcement "Task $task has been added to the TODO list!"
-
-
-/* void printInteger(int aNumber) {
-  print('The number is $aNumber.'); // Print to console.
-}
-
-// This is where the app starts executing.
-void main() {
-  var number = 42; // Declare and initialize a variable.
-  printInteger(number); */
